8ea5ed801a92d5348cd48f3effd92c7383d3be2d
schneli3 <schneli3@students.zhaw.ch>
2024-05-31 12:03:28 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,59 @@
 import streamlit as st
 import logging
+import os
+import gdown
 
 # Set logging level to suppress the special tokens warning
 logging.getLogger("transformers.tokenization_utils_base").setLevel(logging.ERROR)
+
+# Funktion zum Herunterladen der Dateien von Google Drive
+def download_file(url, output):
+    if not os.path.exists(output):
+        gdown.download(url, output, quiet=False)
+
+# Google Drive URLs zu den Modell-Dateien
+model_files = {
+    "bart": {
+        "config.json": "https://drive.google.com/uc?id=1L-bA0x_L3x-5g6zv7bx0x_8rYAIf0Ycu",
+        "generation_config.json": "https://drive.google.com/uc?id=1DQm7qXQu0fr8nzBpwjr6oKy-cZ6-BSwl",
+        "merges.txt": "https://drive.google.com/uc?id=1frNG28RZqCLp2KtP3MA0ibQvhzjPcLtH",
+        "model.safetensors": "https://drive.google.com/uc?id=14A5Xk090NjQWJxJ4Oca7iIIxYBdnz6Kw",
+        "special_tokens_map.json": "https://drive.google.com/uc?id=1XS66vZ-V05PSKtz70PvBYX_rxFozFgzp",
+        "tokenizer_config.json": "https://drive.google.com/uc?id=10Xtby-l0a-VjpKvvIOP8zVd1fPfEeAoT",
+        "vocab.json": "https://drive.google.com/uc?id=1bolVhYKdd4VCgph7_xTbUaxBle4Zf2_W"
+    },
+    "t5_base": {
+        "config.json": "https://drive.google.com/uc?id=1pu0fbzGhAbgQ35D82f_hKiMa7u-YiXOK",
+        "generation_config.json": "https://drive.google.com/uc?id=1hCHgSwSByIoU_kdHZlQsH0xBnY9Jd7hH",
+        "special_tokens_map.json": "https://drive.google.com/uc?id=1OpiuSOzybkEUn_aj6nsFQ_fbgMWHSAxt",
+        "model.safetensors": "https://drive.google.com/uc?id=1Xfx-zFjZP7Xgo13OSh62w4Z9Hwq9YnIL",
+        "added_tokens.json": "https://drive.google.com/uc?id=1gcnNSPX9HZV3tb4EPoXt3OI9k2fE77pG",
+        "spiece.model": "https://drive.google.com/uc?id=165Wij00He548AGeTfC8556_kWUlyRppZ",
+        "tokenizer_config.json": "https://drive.google.com/uc?id=1VVM-8twIRxfuIguD4HUvMS1A6EetsGTQ"
+    },
+    "fine_tuned": {
+        "config.json": "https://drive.google.com/uc?id=1ptB72riEKrQjwfnZDZ4QI1JkSnG40e9A",
+        "generation_config.json": "https://drive.google.com/uc?id=1Zdurv0fhhvJypZJQYQSS2T1WZRJUpYjb",
+        "special_tokens_map.json": "https://drive.google.com/uc?id=1zAy2hTS9Mgj00_Q0x8lchZHwJyWo-g1F",
+        "model.safetensors": "https://drive.google.com/uc?id=1OPWgs1-UeRSpeCmOfPoj4cmQymKMjWer",
+        "added_tokens.json": "https://drive.google.com/uc?id=1glNnIyCp-op7RbmS-8GFXCl4thAgHKA8",
+        "spiece.model": "https://drive.google.com/uc?id=1PcLiN4muOaZbB5sBTE_qrStT-r6b6KkJ",
+        "tokenizer_config.json": "https://drive.google.com/uc?id=1VtOSN74rhuqoYtV084u9qwZ8I6tmBy5y"
+    }
+}
+
+# Lokale Pfade zum Speichern der Modell-Dateien
+model_paths = {
+    "bart": "streamlit_models/bart_model",
+    "t5_base": "streamlit_models/t5_base_model",
+    "fine_tuned": "streamlit_models/fine_tuned_model"
+}
+
+# Erstellen der Verzeichnisse und Herunterladen der Dateien, falls nicht vorhanden
+for model, files in model_files.items():
+    os.makedirs(model_paths[model], exist_ok=True)
+    for file_name, url in files.items():
+        download_file(url, os.path.join(model_paths[model], file_name))
 
 from streamlit_pages.summarizepdf_page import show_summarizepdf_page
 from streamlit_pages.model_info_page import show_model_info_page
@@ -44,7 +95,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Summarize PDFs","Summarize URLs", "Model Info"])
+page = st.sidebar.radio("Go to", ["Home", "Summarize PDFs", "Summarize URLs", "Model Info"])
 
 if page == "Home":
     show_home_page()
--- streamlit_pages/model_info_page.py
+++ streamlit_pages/model_info_page.py
@@ -1,112 +1,18 @@
 import streamlit as st
 from transformers import BartForConditionalGeneration, BartTokenizer, T5ForConditionalGeneration, T5Tokenizer
 import pandas as pd
-import gdown
 import os
 
-# Google Drive IDs zu den BART Modell-Dateien
-bart_config_json_id = "1L-bA0x_L3x-5g6zv7bx0x_8rYAIf0Ycu"
-bart_generation_config_json_id = "1DQm7qXQu0fr8nzBpwjr6oKy-cZ6-BSwl"
-bart_merges_txt_id = "1frNG28RZqCLp2KtP3MA0ibQvhzjPcLtH"
-bart_model_safetensors_id = "14A5Xk090NjQWJxJ4Oca7iIIxYBdnz6Kw"
-bart_special_tokens_map_json_id = "1XS66vZ-V05PSKtz70PvBYX_rxFozFgzp"
-bart_tokenizer_config_json_id = "10Xtby-l0a-VjpKvvIOP8zVd1fPfEeAoT"
-bart_vocab_json_id = "1bolVhYKdd4VCgph7_xTbUaxBle4Zf2_W"
-
-# Google Drive IDs zu den T5 Modell-Dateien
-t5_config_json_id = "1pu0fbzGhAbgQ35D82f_hKiMa7u-YiXOK"
-t5_generation_config_json_id = "1hCHgSwSByIoU_kdHZlQsH0xBnY9Jd7hH"
-t5_special_tokens_map_json_id = "1OpiuSOzybkEUn_aj6nsFQ_fbgMWHSAxt"
-t5_model_safetensors_id = "1Xfx-zFjZP7Xgo13OSh62w4Z9Hwq9YnIL"
-t5_added_tokens_json_id = "1gcnNSPX9HZV3tb4EPoXt3OI9k2fE77pG"
-t5_spiece_model_id = "165Wij00He548AGeTfC8556_kWUlyRppZ"
-t5_tokenizer_config_json_id = "1VVM-8twIRxfuIguD4HUvMS1A6EetsGTQ"
-
-# Google Drive IDs zu den fine-tuned T5 Modell-Dateien
-fine_tuned_config_json_id = "1ptB72riEKrQjwfnZDZ4QI1JkSnG40e9A"
-fine_tuned_generation_config_json_id = "1Zdurv0fhhvJypZJQYQSS2T1WZRJUpYjb"
-fine_tuned_special_tokens_map_json_id = "1zAy2hTS9Mgj00_Q0x8lchZHwJyWo-g1F"
-fine_tuned_model_safetensors_id = "1OPWgs1-UeRSpeCmOfPoj4cmQymKMjWer"
-fine_tuned_added_tokens_json_id = "1glNnIyCp-op7RbmS-8GFXCl4thAgHKA8"
-fine_tuned_spiece_model_id = "1PcLiN4muOaZbB5sBTE_qrStT-r6b6KkJ"
-fine_tuned_tokenizer_config_json_id = "1VtOSN74rhuqoYtV084u9qwZ8I6tmBy5y"
-
-# Google Drive URLs zu den BART Modell-Dateien
-bart_config_json_url = f"https://drive.google.com/uc?id={bart_config_json_id}"
-bart_generation_config_json_url = f"https://drive.google.com/uc?id={bart_generation_config_json_id}"
-bart_merges_txt_url = f"https://drive.google.com/uc?id={bart_merges_txt_id}"
-bart_model_safetensors_url = f"https://drive.google.com/uc?id={bart_model_safetensors_id}"
-bart_special_tokens_map_json_url = f"https://drive.google.com/uc?id={bart_special_tokens_map_json_id}"
-bart_tokenizer_config_json_url = f"https://drive.google.com/uc?id={bart_tokenizer_config_json_id}"
-bart_vocab_json_url = f"https://drive.google.com/uc?id={bart_vocab_json_id}"
-
-# Google Drive URLs zu den T5 Modell-Dateien
-t5_config_json_url = f"https://drive.google.com/uc?id={t5_config_json_id}"
-t5_generation_config_json_url = f"https://drive.google.com/uc?id={t5_generation_config_json_id}"
-t5_special_tokens_map_json_url = f"https://drive.google.com/uc?id={t5_special_tokens_map_json_id}"
-t5_model_safetensors_url = f"https://drive.google.com/uc?id={t5_model_safetensors_id}"
-t5_added_tokens_json_url = f"https://drive.google.com/uc?id={t5_added_tokens_json_id}"
-t5_spiece_model_url = f"https://drive.google.com/uc?id={t5_spiece_model_id}"
-t5_tokenizer_config_json_url = f"https://drive.google.com/uc?id={t5_tokenizer_config_json_id}"
-
-# Google Drive URLs zu den fine-tuned T5 Modell-Dateien
-fine_tuned_config_json_url = f"https://drive.google.com/uc?id={fine_tuned_config_json_id}"
-fine_tuned_generation_config_json_url = f"https://drive.google.com/uc?id={fine_tuned_generation_config_json_id}"
-fine_tuned_special_tokens_map_json_url = f"https://drive.google.com/uc?id={fine_tuned_special_tokens_map_json_id}"
-fine_tuned_model_safetensors_url = f"https://drive.google.com/uc?id={fine_tuned_model_safetensors_id}"
-fine_tuned_added_tokens_json_url = f"https://drive.google.com/uc?id={fine_tuned_added_tokens_json_id}"
-fine_tuned_spiece_model_url = f"https://drive.google.com/uc?id={fine_tuned_spiece_model_id}"
-fine_tuned_tokenizer_config_json_url = f"https://drive.google.com/uc?id={fine_tuned_tokenizer_config_json_id}"
-
-# Lokale Pfade zum Speichern der Modell-Dateien
+# Lokale Pfade zu den Modellen
 bart_model_path = "streamlit_models/bart_model"
 t5_model_path = "streamlit_models/t5_base_model"
 fine_tuned_model_path = "streamlit_models/fine_tuned_model"
 
-# Funktion zum Herunterladen der Dateien von Google Drive
-def download_file(url, output):
-    if not os.path.exists(output):
-        gdown.download(url, output, quiet=False)
-
-# Erstellen der Verzeichnisse und Herunterladen der Dateien, falls nicht vorhanden
-os.makedirs(bart_model_path, exist_ok=True)
-os.makedirs(t5_model_path, exist_ok=True)
-os.makedirs(fine_tuned_model_path, exist_ok=True)
-
-# Download der BART Modell-Dateien
-download_file(bart_config_json_url, os.path.join(bart_model_path, "config.json"))
-download_file(bart_generation_config_json_url, os.path.join(bart_model_path, "generation_config.json"))
-download_file(bart_merges_txt_url, os.path.join(bart_model_path, "merges.txt"))
-download_file(bart_model_safetensors_url, os.path.join(bart_model_path, "model.safetensors"))
-download_file(bart_special_tokens_map_json_url, os.path.join(bart_model_path, "special_tokens_map.json"))
-download_file(bart_tokenizer_config_json_url, os.path.join(bart_model_path, "tokenizer_config.json"))
-download_file(bart_vocab_json_url, os.path.join(bart_model_path, "vocab.json"))
-
-# Download der T5 Modell-Dateien
-download_file(t5_config_json_url, os.path.join(t5_model_path, "config.json"))
-download_file(t5_generation_config_json_url, os.path.join(t5_model_path, "generation_config.json"))
-download_file(t5_special_tokens_map_json_url, os.path.join(t5_model_path, "special_tokens_map.json"))
-download_file(t5_model_safetensors_url, os.path.join(t5_model_path, "model.safetensors"))
-download_file(t5_added_tokens_json_url, os.path.join(t5_model_path, "added_tokens.json"))
-download_file(t5_spiece_model_url, os.path.join(t5_model_path, "spiece.model"))
-download_file(t5_tokenizer_config_json_url, os.path.join(t5_model_path, "tokenizer_config.json"))
-
-# Download der fine-tuned T5 Modell-Dateien
-download_file(fine_tuned_config_json_url, os.path.join(fine_tuned_model_path, "config.json"))
-download_file(fine_tuned_generation_config_json_url, os.path.join(fine_tuned_model_path, "generation_config.json"))
-download_file(fine_tuned_special_tokens_map_json_url, os.path.join(fine_tuned_model_path, "special_tokens_map.json"))
-download_file(fine_tuned_model_safetensors_url, os.path.join(fine_tuned_model_path, "model.safetensors"))
-download_file(fine_tuned_added_tokens_json_url, os.path.join(fine_tuned_model_path, "added_tokens.json"))
-download_file(fine_tuned_spiece_model_url, os.path.join(fine_tuned_model_path, "spiece.model"))
-download_file(fine_tuned_tokenizer_config_json_url, os.path.join(fine_tuned_model_path, "tokenizer_config.json"))
-
 # Laden der Modelle und Tokenizer
 bart_tokenizer = BartTokenizer.from_pretrained(bart_model_path)
 bart_model = BartForConditionalGeneration.from_pretrained(bart_model_path, use_safetensors=True)
-
 t5_tokenizer = T5Tokenizer.from_pretrained(t5_model_path)
 t5_model = T5ForConditionalGeneration.from_pretrained(t5_model_path, use_safetensors=True)
-
 fine_tuned_tokenizer = T5Tokenizer.from_pretrained(fine_tuned_model_path)
 fine_tuned_model = T5ForConditionalGeneration.from_pretrained(fine_tuned_model_path, use_safetensors=True)
 
@@ -148,9 +54,8 @@ def show_model_info_page():
     - **Training Data:** 
         - **T5 Base Model:** Trained on a broad dataset, including the Colossal Clean Crawled Corpus (C4).
         - **Fine-tuned T5 Model:** Initially trained on the same broad dataset as the T5 Base Model, but further trained (fine-tuned) on the XSum dataset, which is a large dataset specifically for summarization tasks.
-
     In summary, while the architecture of both models is the same, the fine-tuned model has learned to better handle summarization tasks due to its specialized training on the XSum dataset.
     """)
-
+    
 if __name__ == "__main__":
     show_model_info_page()
